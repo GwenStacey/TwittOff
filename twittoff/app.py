36aaf7345aea05e9698617597c330647138cf651
GwenStacey <charles.i.purcell@gmail.com>
2019-10-10 19:39:02 -0600

    Added .html files for user and prediction. Added pipenv

--- twittoff/app.py
+++ twittoff/app.py
@@ -1,8 +1,10 @@
 '''Basic app creation'''
 from decouple import config
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user, update_all_users, add_default_users
+from .predict import predict_user
 
 load_dotenv()
 
@@ -18,5 +20,49 @@ def create_app():
         return render_template("base.html",
                                title="Twittoff",
                                users=users)
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.username == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets, message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        tweet_text = request.values['tweet_text']
+
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2, tweet_text)
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], 
+                user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html',
+                               title='Reset database!')
+
+    @app.route('/update')
+    def update():
+        update_all_users()
+        return render_template('base.html',
+                               users=User.query.all(),
+                               title='All Tweets updated!')
 
     return app
--- twittoff/models.py
+++ twittoff/models.py
@@ -1,25 +1,22 @@
+  
 """SQLAlchemy models for TwitOff"""
 from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
 class User(DB.Model):
-    '''Twitter users that we query and store historical tweets'''
-    id = DB.Column(DB.BigInteger,
-                   primary_key=True)
-    username = DB.Column(DB.String(15),
-                         unique=True,
-                         nullable=False)
-    followers = DB.Column(DB.BigInteger,
-                          nullable=False)
-    #Tweet IDs are ordinal ints, so we can fetch most recent
-    newest_tweet_id = DB.Column(DB.BigInteger,
-                                nullable=False)
+    """Twitter users that we query and store historical tweets"""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    username = DB.Column(DB.String(15), unique=True, nullable=False)
+    followers = DB.Column(DB.BigInteger, nullable=False)
+    # Tweets IDs are ordinal ints, so we can fetch most recent tweets
+    newest_tweet_id = DB.Column(DB.BigInteger, nullable=False)
 
 class Tweets(DB.Model):
-    '''Stores tweets'''
-    id = DB.Column(DB.BigInteger,
-                   primary_key=True)
+    """Stores tweets"""
+    id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(280))
-    embedding = DB.Column(DB.PickleType,
-                          nullable=False)
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    
